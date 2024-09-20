969d4b8412d35865f3ca04ce8573a2262d297991
AlexandreTechdriven <alexandre.machado@techdrivendigital.com>
2024-09-20 07:28:58 -0300

    Modularizando

--- include/person.h
+++ include/person.h
@@ -1,14 +1,18 @@
 #ifndef PERSON_H
 #define PERSON_H
 
-#define PERSON_NAME_LEN 120
-#define PERSON_ADDRESS_LEN 120
+#define PERSON_NAME_LEN         120
+#define PERSON_ADDRESS_LEN      120
 
-typedef struct
+typedef struct 
 {
     char name[PERSON_NAME_LEN];
     char address[PERSON_ADDRESS_LEN];
     int age;
 } person_t;
 
-#endif
+person_t person_create(void);
+char *person_input_name(void);
+void person_parser(char *buffer, person_t *person);
+
+#endif /* PERSON_H */
