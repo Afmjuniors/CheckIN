752be37e19b7e6e833749c3927bce69788d6a4be
Alexandre Machado <alexandre.machado@techdrivendigital.com>
2024-09-23 15:23:30 -0300

    WIP:

---
+++ src/repository/sqlite/sqlite_database.h
@@ -0,0 +1,11 @@
+#ifndef SQLITE_DATABASE_H
+#define SQLITE_DATABASE_H
+
+#include <repository.h>
+#include <stdbool.h>
+
+
+
+repository_base *sqlite_create_database(void);
+
+#endif /* SQLITE_DATABASE_H */
